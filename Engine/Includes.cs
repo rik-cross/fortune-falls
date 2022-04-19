559db6d1c523599392ec3d2dbacb9c2305a51305
rik-cross <rik@raspberrypi.org>
2022-04-19 10:06:31 +0100

    Updated .gitignore to include Content files

---
+++ Engine/Includes.cs
@@ -0,0 +1,8 @@
+﻿
+namespace AdventureGame.Engine
+{
+    using Microsoft.Xna.Framework;
+    using MonoGame.Extended.Content;
+    using MonoGame.Extended.Sprites;
+    using MonoGame.Extended.Serialization;
+}
