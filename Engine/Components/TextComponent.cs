fd20fb530be1334b7aa376bb6d268c27911cc1e4
rik-cross <rik@raspberrypi.org>
2022-04-27 19:24:08 +0100

    Multi-line text

--- Engine/Components/TextComponent.cs
+++ Engine/Components/TextComponent.cs
@@ -1,15 +1,71 @@
 ﻿using Microsoft.Xna.Framework;
+using System.Collections;
+using System.Collections.Generic;
 
 namespace AdventureGame.Engine
 {
     public class TextComponent : Component
     {
         public string text;
-        public Color colour;
+        public List<string> splitText;
+        public int textMaxLength;
+        public int singleRowheight;
+        public int totalHeight;
+        public int textMargin;
+        public int outerMargin;
+        public int textWidth;
+
+        public Color textColour;
+        public Color backgroundColour;
+
         public TextComponent(string text)
         {
             this.text = text;
-            this.colour = Color.White;
+            this.textColour = Color.Black;
+            this.backgroundColour = Color.White;
+            this.textMaxLength = 150;
+
+            SplitText();
+
+            this.textMargin = 2;
+            this.outerMargin = 10;
+            this.singleRowheight = ((int)Globals.fontSmall.MeasureString(this.text).Y) + this.textMargin;
+            this.totalHeight = this.singleRowheight * this.splitText.Count-1;
+            this.totalHeight -= this.textMargin;
+
+            this.textWidth = 0;
+            foreach (string line in splitText)
+            {
+                if (Globals.fontSmall.MeasureString(line).X > this.textWidth)
+                    this.textWidth = (int)Globals.fontSmall.MeasureString(line).X;
+            }
+
         }
+
+        public void SplitText()
+        {
+            splitText = new List<string>();
+            string[] words = text.Split();
+            string currentRow = "";
+            foreach (string word in words)
+            {
+
+                if ( Globals.fontSmall.MeasureString(currentRow).X + Globals.fontSmall.MeasureString(word).X < textMaxLength)
+                {
+                    if (currentRow == "")
+                        currentRow += word;
+                    else
+                        currentRow += " " + word;
+                    
+                } else
+                {
+                    splitText.Add(currentRow);
+                    currentRow = word;
+                }
+            }
+            if (currentRow != "")
+                splitText.Add(currentRow);
+        }
+
     }
 }
--- Engine/Systems/TextSystem.cs
+++ Engine/Systems/TextSystem.cs
@@ -14,14 +14,39 @@ namespace AdventureGame.Engine
             if (textComponent == null || transformComponent == null)
                 return;
 
-            Globals.spriteBatch.DrawString(
-                Globals.fontSmall,
-                textComponent.text,
-                new Vector2(
-                    transformComponent.position.X - transformComponent.size.X/2,
-                    transformComponent.position.Y - transformComponent.size.Y/2 - Globals.fontSmall.MeasureString(textComponent.text).Y
-                ), textComponent.colour
+            int rowHeight = textComponent.totalHeight;
+
+            Globals.spriteBatch.FillRectangle(
+                new Rectangle(
+                    (int)(transformComponent.position.X - transformComponent.size.X / 2),
+                    (int)(transformComponent.position.Y - transformComponent.size.Y / 2 - textComponent.totalHeight - (textComponent.outerMargin*2)),
+                    textComponent.textWidth + textComponent.outerMargin * 2,
+                    textComponent.totalHeight + textComponent.outerMargin * 2
+                ),
+                textComponent.backgroundColour
             );
+
+            Globals.spriteBatch.DrawRectangle(
+                new Rectangle(
+                    (int)(transformComponent.position.X - transformComponent.size.X / 2),
+                    (int)(transformComponent.position.Y - transformComponent.size.Y / 2 - textComponent.totalHeight - (textComponent.outerMargin*2)),
+                    textComponent.textWidth + textComponent.outerMargin * 2,
+                    textComponent.totalHeight + textComponent.outerMargin * 2
+                ),
+                textComponent.textColour
+            );
+
+            foreach (string line in textComponent.splitText) {
+                Globals.spriteBatch.DrawString(
+                    Globals.fontSmall,
+                    line,
+                    new Vector2(
+                        transformComponent.position.X - transformComponent.size.X / 2 + textComponent.outerMargin,
+                        transformComponent.position.Y - transformComponent.size.Y / 2 - rowHeight - (textComponent.outerMargin)
+                    ), textComponent.textColour
+                );
+                rowHeight -= textComponent.singleRowheight;
+            }
 
         }
     }
