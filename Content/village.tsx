14aafd802de84efd61cd87c445286cf686e6b3f0
Alex Parry <alex.parry@raspberrypi.org>
2022-08-30 09:07:52 +0100

    Village tileset and map

---
+++ Content/village.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.6" name="Serene_Village_32x32" tilewidth="32" tileheight="32" tilecount="855" columns="19">
+ <image source="villageTileset.png" width="608" height="1440"/>
+</tileset>
