e7892f8e663676d1360f1d1de8f4e8ad2c8c48fc
rik-cross <rik@raspberrypi.org>
2022-09-09 18:44:18 +0100

    Added village tilemap

---
+++ Engine/Dialogue.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+
+namespace AdventureGame.Engine
+{
+    public class Dialogue
+    {
+        public List<string> text = new List<string>();
+        public Entity entity;
+        public Dialogue(string text, Entity entity = null)
+        {
+            this.text.Add(text);
+            this.entity = entity;
+        }
+        public void DrawImage(int x, int y)
+        {
+
+        }
+        public void DrawText(int x, int y)
+        {
+
+        }
+    }
+}
---
+++ Engine/Systems/DialogueSystem.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.Xna.Framework;
+using MonoGame.Extended;
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+using S = System.Diagnostics.Debug;
+
+namespace AdventureGame.Engine
+{
+    public class DialogueSystem : System
+    {
+        private List<Dialogue> dialoguePages = new List<Dialogue>();
+        private int borderSize;
+        private Vector2 size;
+        private Vector2 position;
+        public Color backgroundColour;
+        public Color borderColour;
+        private int borderThickness;
+        public DialogueSystem()
+        {
+            borderSize = 20;
+            size = new Vector2(Globals.WIDTH - (borderSize * 2), 300);
+            position = new Vector2(borderSize, Globals.HEIGHT - size.Y - borderSize);
+            backgroundColour = Color.AntiqueWhite;
+            borderColour = Color.SaddleBrown;
+            borderThickness = 5;
+            aboveMap = true;
+
+            // test
+            this.dialoguePages.Add(new Dialogue("test! hah"));
+        }
+        public override void Draw(GameTime gameTime, Scene scene)
+        {
+            if (dialoguePages.Count == 0)
+                return;
+
+            // Draw background
+            Globals.spriteBatch.FillRectangle(
+                new Rectangle(
+                    (int)position.X, (int)position.Y,
+                    (int)size.X, (int)size.Y
+                ),
+                backgroundColour);
+
+            // Draw border
+            Globals.spriteBatch.DrawRectangle(
+                new Rectangle(
+                    (int)position.X, (int)position.Y,
+                    (int)size.X, (int)size.Y
+                ),
+                borderColour,
+                thickness: borderThickness
+            );
+
+            // Draw dialogue
+            //dialoguePages[0].Draw();
+
+        }
+    }
+}
