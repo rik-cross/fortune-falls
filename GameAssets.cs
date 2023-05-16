56be2352792ed1384316eda7b4ab5951108485a9
rik-cross <rik@raspberrypi.org>
2023-05-16 20:56:00 +0100

    Had a tiny bit of a tidy up

--- GameAssets.cs
+++ GameAssets.cs
@@ -4,10 +4,12 @@ namespace AdventureGame
 {
     public static class GameAssets
     {
-        //public static Texture2D emotes_texture = LoadTexture("../../../Content/Emojis/emotes.png");
+        private static string contentLocation = "../../../Content/";
+
+        // spritesheets
         //public static Engine.SpriteSheet emote_spritesheet = new Engine.SpriteSheet("../../../Content/Emojis/emotes.png");
 
-        public static Texture2D pd = Engine.Utils.LoadTexture("../../../Content/Emojis/emote_pickaxe.png");//Globals.content.Load<Texture2D>("Emojis/emote_pickaxe");
-        public static Engine.Image emote_pickaxe = new Engine.Image(pd);
+        // empte images
+        public static Engine.Image emote_pickaxe = new Engine.Image(Engine.Utils.LoadTexture(contentLocation + "Emojis/emote_pickaxe.png"));
     }
 }
