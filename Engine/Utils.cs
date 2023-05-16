4b9af82d590ef0335d52a980cadd51bbae0a3c4b
rik-cross <rik@raspberrypi.org>
2023-05-16 20:50:06 +0100

    Separated out LoadTexture into new Utils static class

---
+++ Engine/Utils.cs
@@ -0,0 +1,17 @@
+﻿using S = System;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace AdventureGame.Engine
+{
+    public static class Utils
+    {
+        public static Texture2D LoadTexture(string uri)
+        {
+            S.IO.FileStream imageFile = new S.IO.FileStream(uri, S.IO.FileMode.Open, S.IO.FileAccess.Read);
+            Texture2D Image = Texture2D.FromStream(Globals.graphicsDevice, imageFile);
+            imageFile.Close();
+            imageFile = null;
+            return Image;
+        }
+    }
+}
--- GameAssets.cs
+++ GameAssets.cs
@@ -1,12 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-using Microsoft.Xna.Framework;
-using Microsoft.Xna.Framework.Graphics;
-using Microsoft.Xna.Framework.Audio;
-
-using S = System.Diagnostics.Debug;
+﻿using Microsoft.Xna.Framework.Graphics;
 
 namespace AdventureGame
 {
@@ -15,18 +7,7 @@ namespace AdventureGame
         //public static Texture2D emotes_texture = LoadTexture("../../../Content/Emojis/emotes.png");
         //public static Engine.SpriteSheet emote_spritesheet = new Engine.SpriteSheet("../../../Content/Emojis/emotes.png");
 
-        public static Texture2D pd = LoadTexture("../../../Content/Emojis/emote_pickaxe.png");//Globals.content.Load<Texture2D>("Emojis/emote_pickaxe");
+        public static Texture2D pd = Engine.Utils.LoadTexture("../../../Content/Emojis/emote_pickaxe.png");//Globals.content.Load<Texture2D>("Emojis/emote_pickaxe");
         public static Engine.Image emote_pickaxe = new Engine.Image(pd);
-
-        
-        public static Texture2D LoadTexture(string uri)
-        {
-            System.IO.FileStream imageFile = new System.IO.FileStream(uri, System.IO.FileMode.Open, System.IO.FileAccess.Read);
-            Texture2D Image = Texture2D.FromStream(Globals.graphicsDevice, imageFile);
-            imageFile.Close();
-            imageFile = null;
-            return Image;
-        }
     }
- 
 }
