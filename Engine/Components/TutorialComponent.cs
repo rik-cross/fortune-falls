2d7a0bedf2f54ef495186f9ea1ab001f5b587aab
Alex Parry <51757193+alexparry@users.noreply.github.com>
2024-03-11 16:13:51 +0000

    Added ClearTutorials method

--- Engine/Components/TutorialComponent.cs
+++ Engine/Components/TutorialComponent.cs
@@ -7,14 +7,22 @@ namespace AdventureGame.Engine
     public class TutorialComponent : Component
     {
         private List<Tutorial> _tutorialList = new List<Tutorial>();
+
         public void AddTutorial(Tutorial tutorial)
         {
             _tutorialList.Add(tutorial);
         }
+
         public void RemoveTutorial(Tutorial tutorial)
         {
             _tutorialList.Remove(tutorial);
         }
+
+        public void ClearTutorials()
+        {
+            _tutorialList.Clear();
+        }
+
         public List<Tutorial> GetTutorials()
         {
             return _tutorialList;
