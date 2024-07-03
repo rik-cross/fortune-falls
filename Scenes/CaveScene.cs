38b67767583ab9db37a9fa9231316349afe2eb35
Alex Parry <51757193+alexparry@users.noreply.github.com>
2024-07-03 14:48:22 +0100

    Added menu options to cave scene

--- Scenes/CaveScene.cs
+++ Scenes/CaveScene.cs
@@ -59,10 +59,13 @@ namespace AdventureGame
         public override void Input(GameTime gameTime)
         {
             if (EngineGlobals.inputManager.IsPressed(Engine.UIInput.Get("menuPause")))
-            {
-                //EngineGlobals.sceneManager.SetActiveScene<PauseScene>(
-                //    applyTransition: false, unloadCurrentScene: false);
-            }
+                EngineGlobals.sceneManager.ChangeScene<PauseScene>(false);
+
+            if (EngineGlobals.inputManager.IsPressed(Engine.UIInput.Get("menuInventory")))
+                EngineGlobals.sceneManager.ChangeScene<InventoryScene2>(false);
+
+            if (EngineGlobals.inputManager.IsPressed(Engine.UIInput.Get("menuDev")))
+                EngineGlobals.sceneManager.ChangeScene<DevToolsScene>(false);
         }
         public override void Update(GameTime gameTime)
         {
