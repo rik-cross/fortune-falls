06f97c4c9ea44d39dbbc8a25a1047ad03b8c4b6f
Alex Parry <51757193+alexparry@users.noreply.github.com>
2024-11-28 12:36:34 +0000

    Don't ignore GameAssets > SoundEffects

---
+++ GameAssets/SoundEffects.cs
@@ -0,0 +1,12 @@
+﻿using AdventureGame.Engine;
+using Microsoft.Xna.Framework.Audio;
+
+namespace AdventureGame
+{
+    public static class SoundEffects
+    {
+        public static SoundEffect dialogueTickSound = Utils.LoadSoundEffect("Sounds/blip.wav");
+        public static SoundEffect itemPickupSound = Utils.LoadSoundEffect("Sounds/pickup_item.wav");
+        public static SoundEffect footstepSound = Utils.LoadSoundEffect("Sounds/footstep.wav");
+    }
+}
